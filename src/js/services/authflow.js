7fe01bba259f4f07116fed30ca0474c1bc448fee
seiyria <kyle@seiyria.com>
2015-05-26 07:40:46 +0200

    adding facebook auth and some handlers in case it fails

--- src/js/services/authflow.js
+++ src/js/services/authflow.js
@@ -1,10 +1,38 @@
-angular.module('retro').service('AuthFlow', ($state, socket) => {
-    return {
+angular.module('retro').service('AuthFlow', ($q, $ionicHistory, $cordovaToast, $localStorage, $state, socket) => {
+    var flow = {
+        toPlayer: () => {
+            $ionicHistory.nextViewOptions({
+                disableBack: true
+            });
+            $state.go('player');
+        },
         tryAuth: () => {
-            $state.go('create');
+            var fail = () => $state.go('create');
+
+            if($localStorage.facebookId) {
+                flow.login($localStorage).then(null, fail);
+            } else {
+                fail();
+            }
         },
         login: (NewHero) => {
-            socket.emit('login', NewHero);
+            var defer = $q.defer();
+
+            socket.emit('login', NewHero, (err, success) => {
+                if(err) {
+                    defer.reject();
+                } else {
+                    defer.resolve();
+                    flow.toPlayer();
+                }
+
+                var msgObj = err ? err : success;
+                $cordovaToast.showLongBottom(msgObj.msg);
+                console.log(JSON.stringify(err), JSON.stringify(success));
+            });
+
+            return defer.promise;
         }
     };
+    return flow;
 });
