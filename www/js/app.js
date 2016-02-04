2c9ab83a5f3d445dde0bed4313a036f98e6d5c21
seiyria <kyle@seiyria.com>
2016-02-04 11:36:24 -0600

    reorg services

--- www/js/app.js
+++ www/js/app.js
@@ -3,20 +3,6 @@
 angular.module("retro", ["ionic", "ngCordova", "ngStorage", "auth0", "angular-jwt"]);
 "use strict";
 
-angular.module("retro").constant("Config", {
-    _cfg: "DEV",
-    DEV: {
-        url: "192.168.1.7",
-        port: 8080
-    },
-    PROD: {
-        protocol: "https",
-        url: "reactive-retro.herokuapp.com",
-        port: 80
-    }
-});
-"use strict";
-
 angular.module("retro").config(["authProvider", function (authProvider) {
     authProvider.init({
         domain: "reactive-retro.auth0.com",
@@ -219,124 +205,18 @@ angular.module("retro").config(["$ionicConfigProvider", "$urlRouterProvider", "$
 }]);
 "use strict";
 
-angular.module("retro").constant("CLASSES", {
-    Cleric: "Clerics specialize in healing their companions.",
-    Fighter: "Fighters specialize in making their enemies hurt via physical means.",
-    Mage: "Mages specialize in flinging magic at their enemies -- sometimes multiple at once!",
-    Thief: "Thieves specialize in quick attacks and physical debuffing."
-});
-"use strict";
-
-angular.module("retro").constant("OAUTH_KEYS", {
-    google: "195531055167-99jquaolc9p50656qqve3q913204pmnp.apps.googleusercontent.com",
-    reddit: "CKzP2LKr74VwYw",
-    facebook: "102489756752863"
-});
-"use strict";
-
-angular.module("retro").constant("MAP_COLORS", {
-    monster: {
-        outline: "#ff0000",
-        fill: "#aa0000"
+angular.module("retro").constant("Config", {
+    _cfg: "DEV",
+    DEV: {
+        url: "192.168.1.7",
+        port: 8080
     },
-    poi: {
-        outline: "#ffff00",
-        fill: "#aaaa00"
-    },
-    homepoint: {
-        outline: "#00ff00",
-        fill: "#00aa00"
-    },
-    miasma: {
-        outline: "#000000",
-        fill: "#000000"
-    },
-    hero: {
-        outline: "#0000ff",
-        fill: "#0000aa"
-    },
-    heroRadius: {
-        outline: "#ff00ff",
-        fill: "#ff00ff"
+    PROD: {
+        protocol: "https",
+        url: "reactive-retro.herokuapp.com",
+        port: 80
     }
 });
-"use strict";
-
-angular.module("retro").constant("MAP_STYLE", [{
-    featureType: "water",
-    elementType: "geometry",
-    stylers: [{ visibility: "on" }, { color: "#aee2e0" }]
-}, {
-    featureType: "landscape",
-    elementType: "geometry.fill",
-    stylers: [{ color: "#abce83" }]
-}, {
-    featureType: "poi",
-    stylers: [{ visibility: "off" }]
-}, {
-    featureType: "poi.park",
-    elementType: "geometry",
-    stylers: [{ visibility: "simplified" }, { color: "#8dab68" }]
-}, {
-    featureType: "road",
-    elementType: "geometry.fill",
-    stylers: [{ visibility: "simplified" }]
-}, {
-    featureType: "road",
-    elementType: "labels.text.fill",
-    stylers: [{ color: "#5B5B3F" }]
-}, {
-    featureType: "road",
-    elementType: "labels.text.stroke",
-    stylers: [{ color: "#ABCE83" }]
-}, {
-    featureType: "road",
-    elementType: "labels.icon",
-    stylers: [{ visibility: "off" }]
-}, {
-    featureType: "road.local",
-    elementType: "geometry",
-    stylers: [{ color: "#A4C67D" }]
-}, {
-    featureType: "road.arterial",
-    elementType: "geometry",
-    stylers: [{ color: "#9BBF72" }]
-}, {
-    featureType: "road.highway",
-    elementType: "geometry",
-    stylers: [{ color: "#EBF4A4" }]
-}, {
-    featureType: "transit",
-    stylers: [{ visibility: "off" }]
-}, {
-    featureType: "administrative",
-    elementType: "geometry.stroke",
-    stylers: [{ visibility: "on" }, { color: "#87ae79" }]
-}, {
-    featureType: "administrative",
-    elementType: "geometry.fill",
-    stylers: [{ color: "#7f2200" }, { visibility: "off" }]
-}, {
-    featureType: "administrative",
-    elementType: "labels.text.stroke",
-    stylers: [{ color: "#ffffff" }, { visibility: "on" }, { weight: 4.1 }]
-}, {
-    featureType: "administrative",
-    elementType: "labels.text.fill",
-    stylers: [{ color: "#495421" }]
-}, {
-    featureType: "administrative.neighborhood",
-    elementType: "labels",
-    stylers: [{ visibility: "off" }]
-}, {
-    featureType: "administrative.land_parcel",
-    elementType: "labels",
-    stylers: [{ visibility: "off" }]
-}, {
-    featureType: "administrative.locality",
-    elementType: "labels",
-    stylers: [{ visibility: "off" }]
-}]);
 "use strict";
 
 angular.module("retro").controller("ClassChangeController", ["$scope", "Player", "CLASSES", "ClassChangeFlow", function ($scope, Player, CLASSES, ClassChangeFlow) {
@@ -560,6 +440,126 @@ angular.module("retro").controller("SkillChangeController", ["$scope", "$ionicMo
 }]);
 "use strict";
 
+angular.module("retro").constant("CLASSES", {
+    Cleric: "Clerics specialize in healing their companions.",
+    Fighter: "Fighters specialize in making their enemies hurt via physical means.",
+    Mage: "Mages specialize in flinging magic at their enemies -- sometimes multiple at once!",
+    Thief: "Thieves specialize in quick attacks and physical debuffing."
+});
+"use strict";
+
+angular.module("retro").constant("OAUTH_KEYS", {
+    google: "195531055167-99jquaolc9p50656qqve3q913204pmnp.apps.googleusercontent.com",
+    reddit: "CKzP2LKr74VwYw",
+    facebook: "102489756752863"
+});
+"use strict";
+
+angular.module("retro").constant("MAP_COLORS", {
+    monster: {
+        outline: "#ff0000",
+        fill: "#aa0000"
+    },
+    poi: {
+        outline: "#ffff00",
+        fill: "#aaaa00"
+    },
+    homepoint: {
+        outline: "#00ff00",
+        fill: "#00aa00"
+    },
+    miasma: {
+        outline: "#000000",
+        fill: "#000000"
+    },
+    hero: {
+        outline: "#0000ff",
+        fill: "#0000aa"
+    },
+    heroRadius: {
+        outline: "#ff00ff",
+        fill: "#ff00ff"
+    }
+});
+"use strict";
+
+angular.module("retro").constant("MAP_STYLE", [{
+    featureType: "water",
+    elementType: "geometry",
+    stylers: [{ visibility: "on" }, { color: "#aee2e0" }]
+}, {
+    featureType: "landscape",
+    elementType: "geometry.fill",
+    stylers: [{ color: "#abce83" }]
+}, {
+    featureType: "poi",
+    stylers: [{ visibility: "off" }]
+}, {
+    featureType: "poi.park",
+    elementType: "geometry",
+    stylers: [{ visibility: "simplified" }, { color: "#8dab68" }]
+}, {
+    featureType: "road",
+    elementType: "geometry.fill",
+    stylers: [{ visibility: "simplified" }]
+}, {
+    featureType: "road",
+    elementType: "labels.text.fill",
+    stylers: [{ color: "#5B5B3F" }]
+}, {
+    featureType: "road",
+    elementType: "labels.text.stroke",
+    stylers: [{ color: "#ABCE83" }]
+}, {
+    featureType: "road",
+    elementType: "labels.icon",
+    stylers: [{ visibility: "off" }]
+}, {
+    featureType: "road.local",
+    elementType: "geometry",
+    stylers: [{ color: "#A4C67D" }]
+}, {
+    featureType: "road.arterial",
+    elementType: "geometry",
+    stylers: [{ color: "#9BBF72" }]
+}, {
+    featureType: "road.highway",
+    elementType: "geometry",
+    stylers: [{ color: "#EBF4A4" }]
+}, {
+    featureType: "transit",
+    stylers: [{ visibility: "off" }]
+}, {
+    featureType: "administrative",
+    elementType: "geometry.stroke",
+    stylers: [{ visibility: "on" }, { color: "#87ae79" }]
+}, {
+    featureType: "administrative",
+    elementType: "geometry.fill",
+    stylers: [{ color: "#7f2200" }, { visibility: "off" }]
+}, {
+    featureType: "administrative",
+    elementType: "labels.text.stroke",
+    stylers: [{ color: "#ffffff" }, { visibility: "on" }, { weight: 4.1 }]
+}, {
+    featureType: "administrative",
+    elementType: "labels.text.fill",
+    stylers: [{ color: "#495421" }]
+}, {
+    featureType: "administrative.neighborhood",
+    elementType: "labels",
+    stylers: [{ visibility: "off" }]
+}, {
+    featureType: "administrative.land_parcel",
+    elementType: "labels",
+    stylers: [{ visibility: "off" }]
+}, {
+    featureType: "administrative.locality",
+    elementType: "labels",
+    stylers: [{ visibility: "off" }]
+}]);
+"use strict";
+
 angular.module("retro").directive("colorText", function () {
     return {
         restrict: "E",
@@ -659,131 +659,6 @@ angular.module("retro").service("Auth", ["$localStorage", "$state", "$ionicHisto
 
     return localAuth;
 }]);
-"use strict";
-
-angular.module("retro").service("AuthFlow", ["$q", "$rootScope", "$ionicHistory", "Toaster", "$localStorage", "$state", "Player", "Settings", "LocationWatcher", "Config", "socket", function ($q, $rootScope, $ionicHistory, Toaster, $localStorage, $state, Player, Settings, LocationWatcher, Config, socket) {
-    var flow = {
-        toPlayer: function () {
-            if (!_.contains(["home", "create"], $state.current.name)) {
-                return;
-            }
-
-            $ionicHistory.nextViewOptions({
-                disableBack: true
-            });
-            $state.go("player");
-        },
-        tryAuth: function () {
-            var fail = function () {
-                return $state.go("create");
-            };
-
-            if ($localStorage.profile.user_id) {
-                // jshint ignore:line
-                flow.login(_.clone($localStorage), true).then(null, fail);
-
-                //only fail to the char create screen if there's a server connection
-            } else if ($rootScope.canConnect) {
-                fail();
-            }
-        },
-        login: function (NewHeroProto) {
-            var swallow = arguments[1] === undefined ? false : arguments[1];
-
-            var defer = $q.defer();
-
-            var NewHero = {
-                name: NewHeroProto.name,
-                profession: NewHeroProto.profession,
-                userId: NewHeroProto.profile.user_id, //jshint ignore:line
-                token: NewHeroProto.token
-            };
-
-            var currentLocation = LocationWatcher.current();
-            if (!currentLocation) {
-                $rootScope.attemptAutoLogin = false;
-                return Toaster.show("No current location. Is your GPS on?");
-            }
-
-            NewHero.homepoint = { lat: currentLocation.latitude, lon: currentLocation.longitude };
-
-            socket.emit("login", NewHero, function (err, success) {
-                if (err) {
-                    defer.reject();
-                } else {
-                    defer.resolve();
-                    _.extend(Settings, success.settings);
-                    Settings.places = success.places;
-                    Settings.monsters = success.monsters;
-                    flow.toPlayer();
-                    flow.isLoggedIn = true;
-                    $localStorage.env = Config._cfg;
-                }
-
-                $rootScope.attemptAutoLogin = false;
-
-                if (!swallow) {
-                    var msgObj = err ? err : success;
-                    Toaster.show(msgObj.msg);
-                }
-            });
-
-            Settings.isReady = defer.promise;
-            return Settings.isReady;
-        }
-    };
-    return flow;
-}]);
-"use strict";
-
-angular.module("retro").service("ClassChangeFlow", ["Toaster", "$state", "Player", "socket", function (Toaster, $state, Player, socket) {
-    return {
-        change: function (newProfession) {
-
-            var player = Player.get();
-
-            var opts = { name: player.name, newProfession: newProfession };
-            socket.emit("player:change:class", opts, function (err, success) {
-                var msgObj = err ? err : success;
-                Toaster.show(msgObj.msg);
-
-                if (success) {
-                    $state.go("player");
-                }
-            });
-        }
-    };
-}]);
-"use strict";
-
-angular.module("retro").service("Dice", ["$window", function ($window) {
-    return $window.dice;
-}]);
-"use strict";
-
-angular.module("retro").service("EquipFlow", ["Toaster", "$state", "Player", "socket", function (Toaster, $state, Player, socket) {
-    return {
-        equip: function (newItem) {
-
-            var player = Player.get();
-
-            var opts = { name: player.name, itemId: newItem.itemId };
-            socket.emit("player:change:equipment", opts, function (err, success) {
-                var msgObj = err ? err : success;
-                Toaster.show(msgObj.msg);
-
-                if (success) {
-                    $state.go("player");
-                }
-            });
-        }
-    };
-}]);
-"use strict";
-
-angular.module("retro").service("Google", function () {
-    return window.google;
-});
 "use strict";
 
 angular.module("retro").service("LocationWatcher", ["$q", function ($q) {
@@ -1017,6 +892,22 @@ angular.module("retro").service("NewHero", function () {
 });
 "use strict";
 
+angular.module("retro").service("Settings", function () {});
+"use strict";
+
+angular.module("retro").service("Toaster", ["$cordovaToast", function ($cordovaToast) {
+    return {
+        show: function (msg) {
+            try {
+                $cordovaToast.showLongBottom(msg);
+            } catch (e) {
+                console.log(msg);
+            }
+        }
+    };
+}]);
+"use strict";
+
 angular.module("retro").service("Player", ["$q", function ($q) {
     //var clamp = (min, cur, max) => Math.max(min, Math.min(max, cur));
 
@@ -1042,25 +933,6 @@ angular.module("retro").service("Player", ["$q", function ($q) {
 }]);
 "use strict";
 
-angular.module("retro").service("Settings", function () {});
-"use strict";
-
-angular.module("retro").service("SkillChangeFlow", ["Toaster", "$state", "Player", "socket", function (Toaster, $state, Player, socket) {
-    return {
-        change: function (skill, slot) {
-
-            var player = Player.get();
-
-            var opts = { name: player.name, skillName: skill, skillSlot: slot };
-            socket.emit("player:change:skill", opts, function (err, success) {
-                var msgObj = err ? err : success;
-                Toaster.show(msgObj.msg);
-            });
-        }
-    };
-}]);
-"use strict";
-
 angular.module("retro").service("Skills", ["$q", function ($q) {
 
     var defer = $q.defer();
@@ -1080,6 +952,147 @@ angular.module("retro").service("Skills", ["$q", function ($q) {
         }
     };
 }]);
+"use strict";
+
+angular.module("retro").service("AuthFlow", ["$q", "$rootScope", "$ionicHistory", "Toaster", "$localStorage", "$state", "Player", "Settings", "LocationWatcher", "Config", "socket", function ($q, $rootScope, $ionicHistory, Toaster, $localStorage, $state, Player, Settings, LocationWatcher, Config, socket) {
+    var flow = {
+        toPlayer: function () {
+            if (!_.contains(["home", "create"], $state.current.name)) {
+                return;
+            }
+
+            $ionicHistory.nextViewOptions({
+                disableBack: true
+            });
+            $state.go("player");
+        },
+        tryAuth: function () {
+            var fail = function () {
+                return $state.go("create");
+            };
+
+            if ($localStorage.profile.user_id) {
+                // jshint ignore:line
+                flow.login(_.clone($localStorage), true).then(null, fail);
+
+                //only fail to the char create screen if there's a server connection
+            } else if ($rootScope.canConnect) {
+                fail();
+            }
+        },
+        login: function (NewHeroProto) {
+            var swallow = arguments[1] === undefined ? false : arguments[1];
+
+            var defer = $q.defer();
+
+            var NewHero = {
+                name: NewHeroProto.name,
+                profession: NewHeroProto.profession,
+                userId: NewHeroProto.profile.user_id, //jshint ignore:line
+                token: NewHeroProto.token
+            };
+
+            var currentLocation = LocationWatcher.current();
+            if (!currentLocation) {
+                $rootScope.attemptAutoLogin = false;
+                return Toaster.show("No current location. Is your GPS on?");
+            }
+
+            NewHero.homepoint = { lat: currentLocation.latitude, lon: currentLocation.longitude };
+
+            socket.emit("login", NewHero, function (err, success) {
+                if (err) {
+                    defer.reject();
+                } else {
+                    defer.resolve();
+                    _.extend(Settings, success.settings);
+                    Settings.places = success.places;
+                    Settings.monsters = success.monsters;
+                    flow.toPlayer();
+                    flow.isLoggedIn = true;
+                    $localStorage.env = Config._cfg;
+                }
+
+                $rootScope.attemptAutoLogin = false;
+
+                if (!swallow) {
+                    var msgObj = err ? err : success;
+                    Toaster.show(msgObj.msg);
+                }
+            });
+
+            Settings.isReady = defer.promise;
+            return Settings.isReady;
+        }
+    };
+    return flow;
+}]);
+"use strict";
+
+angular.module("retro").service("ClassChangeFlow", ["Toaster", "$state", "Player", "socket", function (Toaster, $state, Player, socket) {
+    return {
+        change: function (newProfession) {
+
+            var player = Player.get();
+
+            var opts = { name: player.name, newProfession: newProfession };
+            socket.emit("player:change:class", opts, function (err, success) {
+                var msgObj = err ? err : success;
+                Toaster.show(msgObj.msg);
+
+                if (success) {
+                    $state.go("player");
+                }
+            });
+        }
+    };
+}]);
+"use strict";
+
+angular.module("retro").service("EquipFlow", ["Toaster", "$state", "Player", "socket", function (Toaster, $state, Player, socket) {
+    return {
+        equip: function (newItem) {
+
+            var player = Player.get();
+
+            var opts = { name: player.name, itemId: newItem.itemId };
+            socket.emit("player:change:equipment", opts, function (err, success) {
+                var msgObj = err ? err : success;
+                Toaster.show(msgObj.msg);
+
+                if (success) {
+                    $state.go("player");
+                }
+            });
+        }
+    };
+}]);
+"use strict";
+
+angular.module("retro").service("SkillChangeFlow", ["Toaster", "$state", "Player", "socket", function (Toaster, $state, Player, socket) {
+    return {
+        change: function (skill, slot) {
+
+            var player = Player.get();
+
+            var opts = { name: player.name, skillName: skill, skillSlot: slot };
+            socket.emit("player:change:skill", opts, function (err, success) {
+                var msgObj = err ? err : success;
+                Toaster.show(msgObj.msg);
+            });
+        }
+    };
+}]);
+"use strict";
+
+angular.module("retro").service("Dice", ["$window", function ($window) {
+    return $window.dice;
+}]);
+"use strict";
+
+angular.module("retro").service("Google", function () {
+    return window.google;
+});
 "use strict";
 
 angular.module("retro").service("socketCluster", ["$window", function ($window) {
@@ -1119,19 +1132,6 @@ angular.module("retro").service("socketCluster", ["$window", function ($window) 
         setUpEvents: function (socket) {
             socket.on("update:player", Player.set);
             socket.on("update:skills", Skills.set);
-        }
-    };
-}]);
-"use strict";
-
-angular.module("retro").service("Toaster", ["$cordovaToast", function ($cordovaToast) {
-    return {
-        show: function (msg) {
-            try {
-                $cordovaToast.showLongBottom(msg);
-            } catch (e) {
-                console.log(msg);
-            }
         }
     };
 }]);
