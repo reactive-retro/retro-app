dac50acaf0c0935dd08bad4aa57f8d1ba380456e
seiyria <kyle@seiyria.com>
2016-04-10 08:37:30 -0500

    made initial dev gulp run less jumpy

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,15 @@
 
 require('babel-register');
 var gulp = require('gulp');
+var runSequence = require('run-sequence');
 
 require('./gulp/build');
 require('./gulp/dev');
 require('./gulp/version');
 
-gulp.task('default', ['sass', 'lib', 'html', 'copy:assets', 'build:dev', 'ionic:start', 'watch']);
+gulp.task('default', (done) => {
+    runSequence(['sass', 'lib', 'html', 'copy:assets', 'build:dev'], 'ionic:start', 'watch');
+    done();
+});
 gulp.task('test', ['build:dev']);
 gulp.task('build:production', ['build:production:compile', 'build:production:apk']);
